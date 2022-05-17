6b8907e6f0837d1203346e39146785b9d16508cb
Anders Børjesson <anbo@zealand.dk>
2022-05-17 14:15:52 +0200

    Add project files.

---
+++ FestivalSelenium/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium; // nuget Selenium.WebDriver
+using OpenQA.Selenium.Chrome;
+
+string driverDirectory = "C:\\webDrivers";
+IWebDriver driver = new ChromeDriver(driverDirectory);
+
+string[] names = {
+    /*"amager-bryghus", "bicycle-brewing",*/ "bryggeriet-kant", "bryggeriudstyr-aps", "castle-malting",
+    "charlies-pubs", "copenhagen-mead-company", "dry-bitter-brewing", "esrum-kloster",
+    "flying-couch-brewing", "gamma-brewing-company", "herslev-bryghus", "husbryggeriet-jacobsen",
+    "haandbryggerne", "observatoriet", "to-oel-cph", "ukrainerejser-og-thunderbrew", "aaben"
+};
+
+foreach (string name in names)
+{
+    string url = $"https://xn--lfestival-k8a.dk/udstillere/{name}/?oelfestival=2022";
+    driver.Navigate().GoToUrl(url);
+
+    try
+    {
+        IWebElement element = driver.FindElement(By.ClassName("tomliste"));
+    }
+    catch (NoSuchElementException) { Console.WriteLine("Found: " + name); }
+}
+
+driver.Dispose();
